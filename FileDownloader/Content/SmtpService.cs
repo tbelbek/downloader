bd2d9b1351028cb327827bfe4ac94cc0348edd77
tughan <Ttughanbelbek@gmail.com>
2018-12-19 08:37:26 +0300

    init

---
+++ FileDownloader/Content/SmtpService.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Mail;
+
+namespace WebUI.Helpers
+{
+    public class SmtpService
+    {
+        private string SenderMail { get; set; }
+        private string SenderPass { get; set; }
+        private SmtpClient Client { get; set; }
+        private bool IsSending { get; set; }
+        //public EmailDeliverySoapClient MailClient { get; set; }
+        //public string MailAccessToken { get; set; }
+
+        public SmtpService()
+        {
+            Client = new SmtpClient
+            {
+                Port = Convert.ToInt32("587"),
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                EnableSsl = true,
+                Host = "smtp.gmail.com"
+            };
+            SenderMail = "tughanbelbek@gmail.com";
+            SenderPass = "pzxbwcjbyjruppjm";
+            //this.MailAccessToken = ConfigurationManager.AppSettings["MailAccessToken"];
+            Client.Credentials = new NetworkCredential(SenderMail, SenderPass);
+            //MailClient = new MailDeliveryService.EmailDeliverySoapClient();
+        }
+
+        public bool SendMail()
+        {
+            return SendMail("test", "test", "tughan.belbek@antasya.com");
+        }
+
+        public bool SendMail(string header, string body, string contactToSend)
+        {
+            IsSending = true;
+            bool result = false;
+            try
+            {
+                result = MailJob(header, body, contactToSend);
+                IsSending = false;
+            }
+            catch (Exception ex)
+            {
+                //FileLogging.FileWriter(ex.Message);
+            }
+            return result;
+        }
+
+        public bool SendMail(string header, string body, List<string> contactsToSend)
+        {
+            IsSending = true;
+            bool result = false;
+            try
+            {
+                contactsToSend.ForEach(x => { result = MailJob(header, body, x); });
+                IsSending = false;
+            }
+            catch (Exception ex)
+            {
+                //FileLogging.FileWriter(ex.Message);
+            }
+            return result;
+        }
+
+        private bool MailJob(string header, string body, string x)
+        {
+            bool result;
+            MailMessage mail = new MailMessage(SenderMail, x);
+            mail.Subject = header;
+            mail.Body = body;
+            mail.IsBodyHtml = true;
+            Client.Send(mail);
+
+            //MailClient.DeliveryBulkEmailNewList_WithArray();
+            result = true;
+            return result;
+        }
+    }
+}
---
+++ FileDownloader/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using Flurl.Http;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using WebUI.Helpers;
+
+namespace FileDownloader.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Download(DownloadData data)
+        {
+            var service = new SmtpService();
+            if (data.Password != ConfigurationSettings.AppSettings["Password"])
+            {
+                service.SendMail("Info", $"Malicious download attempt ", "destructer9@gmail.com");
+            }
+
+            try
+            {
+                ViewBag.Info = "Download started";
+                if (string.IsNullOrEmpty(data.Path))
+                {
+                    data.Path = ConfigurationSettings.AppSettings["DownloadDefaultPath"];
+                }
+                await data.Url.DownloadFileAsync(data.Path);
+                service.SendMail("Info", $"Download Completed for {data.Url}", "destructer9@gmail.com");
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Info = "Download error";
+                service.SendMail("Info", $"Download Failed for {data.Url}, Exception {ex.Message}", "destructer9@gmail.com");
+            }
+            return View("Index");
+        }
+    }
+    public class DownloadData
+    {
+        public string Url { get; set; }
+        public string Password { get; set; }
+        public string Path { get; set; }
+    }
+}
